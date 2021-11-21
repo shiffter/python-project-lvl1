7c617452fcbf9caf98506d0184a374df79a32a74
shifter <vanfre93@gmail.com>
2021-11-21 20:44:08 +0300

    individual logic for each game

---
+++ brain_games/games/brain_even.py
@@ -0,0 +1,15 @@
+import random
+
+
+RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+
+def get_required_values ():
+    value_one = random.randint(1,100)
+    task = ("Question: {}".format(value_one))
+    if value_one % 2 == 0:
+        right_answer = ('Yes')
+    else:
+        right_answer = ('No')
+    return task, right_answer
+
