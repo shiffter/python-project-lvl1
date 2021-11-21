4277f825ded4da7ced57f369ccdd923ec733af38
shifter <vanfre93@gmail.com>
2021-11-21 20:42:09 +0300

    refactor main func

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,70 +1,11 @@
-import random
-import prompt
-from brain_games.cli import welcome_user
-
-
-name = welcome_user()
+#!/usr/bin/env python3
+from brain_games.engine import engine
+from brain_games.games import brain_calc
 
 
 def main():
-    flag = 0
-    counter = 0
-    while counter < 3:
-        value_one, value_two, random_value = get_random_value()
-        right_answer = make_example(random_value, value_one, value_two)
-        user_answer = ask_user()
-        counter, flag = compare_answers(counter, user_answer, value_one, value_two, right_answer, random_value, flag)
-        if counter == 3:
-            print('Congratulations, {}!'.format(name))
-        if flag == 1:
-            break
-
-
-def get_random_value():
-    value_one = random.randrange(0, 100)
-    value_two = random.randrange(0, 1000)
-    random_value = random.randrange(1, 4)
-    return value_one, value_two, random_value
-
-
-def make_example(random_value, value_one, value_two):
-    if random_value == 1:
-        print("Question: {} + {}".format(value_one, value_two))
-        right_answer = value_one + value_two
-        return right_answer
-    elif random_value == 2:
-        print("Question: {} - {}".format(value_one, value_two))
-        right_answer = value_one - value_two
-        return right_answer
-    elif random_value == 3:
-        print("Question: {} * {}".format(value_one, value_two))
-        right_answer = value_one * value_two
-        return right_answer
-
-
-def ask_user():
-    user_answer = prompt.string("Your answer: ")
-    return user_answer
-
-
-def compare_answers(counter, user_answer, value_one, value_two, right_answer, random_value, flag):
-    if user_answer == str(value_one + value_two) and random_value == 1:
-        print('Correct!')
-        counter += 1
-        return counter, flag
-    elif user_answer == str(value_one - value_two) and random_value == 2:
-        print('Correct!')
-        counter += 1
-        return counter, flag
-    elif user_answer == str(value_one * value_two) and random_value == 3:
-        print('Correct!')
-        counter += 1
-        return counter, flag
-    else:
-        print("{} is wrong answer ;(. Correct answer was {}. \nLet's try again, {}!".format(user_answer, right_answer, name))
-        flag += 1
-        return counter, flag
+    engine(brain_calc)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,55 +1,11 @@
-from random import randint
-import prompt
-from brain_games.cli import welcome_user
-
-
-def get_random_value():
-    return randint(0, 666)
-
-
-def make_example(value):
-    print("Question: {}".format(value))
-
-
-def ask_user():
-    user_answer = prompt.string("Your answer: ")
-    return user_answer
-
-
-def compare_answers(counter, user_answer, right_answer, flag=0):
-    if user_answer in right_answer:
-        print('Correct!')
-        counter += 1
-        return counter, flag
-    if user_answer not in right_answer:
-        flag = 1
-        return counter, flag
-
-
-def play_games(value):
-	if value % 2 == 0:
-		right_answer = ('Yes','yes')
-	else:
-		right_answer = ('No', 'no')
-	return right_answer
+#!/usr/bin/env python3
+from brain_games.engine import engine
+from brain_games.games import brain_even
 
 
 def main():
-	name = welcome_user()
-	counter = 0
-	print('Answer "Yes" if given number is even. Otherwise answer "No".')
-	while counter < 3:
-		value = get_random_value()
-		make_example(value)
-		right_answer = play_games(value)
-		user_answer = ask_user()
-		counter, flag = compare_answers(counter, user_answer, right_answer)
-		if counter == 3:
-			return ('Congratulations, {}!'.format(name))
-		if flag == 1:
-			print("{} is wrong answer ;(. Correct answer was {}. \nLet's try again, {}!".format(user_answer, right_answer[1], name))
-			break
+    engine(brain_even)
 
 
 if __name__ == '__main__':
-	main()
+    main()
